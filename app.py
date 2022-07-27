9de283fea182b81257727ba5121bc025a35f7609
Chris HS <chris.hollidaysmith@travelpartners.com.au>
2022-07-28 01:11:49 +1000

    - Added options for user to sort their results

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, redirect, request, session
 from hash import hash, check
 from psql import sql_select, sql_write,get_secret_key
-from service import get_questions, user_logged_in, fetch_data
+from service import get_questions, user_logged_in, fetch_data, reset_password
+from psycopg2.extensions import AsIs 
 
 app = Flask(__name__)
 app.secret_key = get_secret_key().encode()
@@ -12,6 +13,7 @@ def index():
     if user_logged_in():
         return redirect('/home')
     forgot = request.args.get('forgot', '')
+    print(f'$$$$$$${forgot} $$$$$$')
     stage = "1"
     return render_template('index.html', forgot=forgot, stage=stage)
 
@@ -109,16 +111,29 @@ def home():
     if user_logged_in():
         query = sql_select('SELECT card_id FROM achievements WHERE user_id=%s', session['user_id'])
         if query:
-            card_list = []
+            card_list = []            
             for card in query:
-                data = sql_select('SELECT * FROM cards WHERE card_id=%s ORDER BY id', card)
+                data = sql_select('SELECT * FROM cards WHERE card_id=%s', card)
                 card_list.append({
+                    'id': data[0][0],
                     'card_id': data[0][1],
                     'name': data[0][2],
                     'description': data[0][3],
                     'url': data[0][4],
                     'link': data[0][5]
                     })
+            #Check for filter to order results            
+            filters = request.args.get('filter', 'card_id/False')
+            type = filters.split('/')[0]
+            direction = filters.split('/')[1]
+            if direction == 'False':
+                direction = False
+            else:
+                direction = True
+            print(f'$$$$$$$$$$ SORT BY: {type} $$$$$$$')
+            print(f'$$$$$$$$$$ REVERSE: {direction} $$$$$$$')
+            card_list.sort(key=lambda x: x.get(f'{type}'), reverse=direction)
+
             count = sql_select('SELECT count(*) FROM achievements WHERE user_id=%s', session['user_id'])[0][0]
             return render_template('home.html', card_list=card_list, count=count)
         msg="You have not earnt any cards..."        
@@ -126,7 +141,7 @@ def home():
     else:
         return redirect('/')
 
-# Use external API to populate page
+# Fetch data from external API and randomise choices
 @app.route('/play')
 def play():
     if user_logged_in():
@@ -139,6 +154,7 @@ def play():
     else:
         return redirect('/')
 
+# If users guess is correct, add their new card to the cards table and link to achievements Table
 @app.route('/play_action', methods=['POST'])
 def play_action():
     correct = request.form.get('clicked')
@@ -148,18 +164,72 @@ def play_action():
         description = request.form.get('description')
         link = request.form.get('link')
         image = request.form.get('image')
-        sql_write('INSERT INTO cards(card_id, name, description, image, link) VALUES(%s,%s,%s,%s,%s)', card_id, name, description, image, link)
+        card_exists = sql_select('SELECT * FROM cards WHERE card_id=%s', card_id)
+        if not card_exists:
+            sql_write('INSERT INTO cards(card_id, name, description, image, link) VALUES(%s,%s,%s,%s,%s)', card_id, name, description, image, link)
         sql_write('INSERT INTO achievements(user_id, card_id) VALUES(%s,%s)', session['user_id'], card_id)        
     return redirect('/play')    
 
 # Allow anyone with the admin title to edit or delete other users
 @app.route('/admin')
-def admin():
+def admin():    
     if user_logged_in():
-        users = sql_select('SELECT * FROM users', None)
+        users = sql_select('SELECT * FROM users ORDER BY user_id', None)
         return render_template('admin.html', users=users)
     else:
         return redirect('/')
+
+@app.route('/delete_user', methods=["POST"])
+def delete_action():
+    id = request.form.get('id', '')
+    confirmed = request.form.get('confirm_delete', '')
+    if confirmed:
+        sql_write('DELETE FROM achievements WHERE user_id=%s', id)
+        sql_write('DELETE FROM users WHERE user_id=%s', id)
+        return redirect('/admin')
+    if id:
+        users = sql_select('SELECT * FROM users WHERE user_id=%s', id)
+        return render_template('admin.html', id=id, users=users, confirm_delete=True)
+    else:
+        msg = 'An error has occured'
+        return render_template("admin.html", msg=msg)
+
+@app.route('/edit_user', methods=["POST"])
+def edit_user():
+    id = request.form.get('id', '')
+    if id:
+        users = sql_select('SELECT * FROM users WHERE user_id=%s', id)
+        return render_template('admin.html', users=users, edit_user=True)
+
+@app.route('/edit_action', methods=["POST"])
+def edit_action():
+    id = request.form.get('id', '')
+    reset = request.form.get('reset', '')
+    wipe = request.form.get('wipe', '')
+    remove = request.form.get('remove_admin', '')
+    add = request.form.get('make_admin', '')
+    msg = ''
+    if reset:
+        new_password = reset_password()
+        msg += f'\nNEW PASSWORD SET TO: {new_password}'
+        new_password = hash(new_password)
+        sql_write('UPDATE users SET password=%s WHERE user_id=%s', new_password, id)
+    if wipe:
+        sql_write('DELETE FROM achievements WHERE user_id=%s', id)
+        msg += f'\nCARDS WIPED FROM USER ID: {id}'
+    if remove:
+        admin_count = sql_select('SELECT count(admin) FROM users WHERE admin=True')[0][0]
+        if admin_count == 1:
+            msg += '\nCANNOT REMOVE ADMIN - You must have at least one active admin!'
+        else:
+            sql_write('UPDATE users SET admin=False WHERE user_id=%s', id)
+            msg += f'\nADMIN REMOVED FROM USER ID: {id}'
+    if add:
+        sql_write('UPDATE users SET admin=True WHERE user_id=%s', id)
+        msg += f'\nADMIN ADDED TO USER ID: {id}'
+    users = sql_select('SELECT * FROM users', None)
+    return render_template('admin.html', users=users, msg=msg)
+
 
 # Modal popup for user preferences
 @app.route('/settings')
